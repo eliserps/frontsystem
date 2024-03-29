29321b3d39bdb3a04583549682afc1941f879a93
Elise Silva <elisedpaula@gmail.com>
2024-03-28 22:17:03 -0300

    Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SignupComponent } from './src/pages/signup/signup.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SignupComponent } from "./pages/signup/signup.component";
 
 @NgModule({
-declarations: [
-AppComponent,
-SignupComponent
-],
-imports: [
-BrowserModule,
-AppRoutingModule
-],
-providers: [],
-bootstrap: [AppComponent]
+  declarations: [AppComponent, SignupComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
