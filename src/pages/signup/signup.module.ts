ab6d0ed2c32b8bb7a19b97745ff9ad80346977a5
Leonardo <leopaulasilva83@gmail.com>
2024-03-28 21:52:10 -0300

    Elise

---
+++ src/pages/signup/signup.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { SignupComponent } from './src/pages/signup/signup.component';
+
+@NgModule({
+declarations: [
+  SignupComponent
+],
+imports: [
+  CommonModule
+],
+export: [SignupComponent]
+})
+export class SignupModule { }
